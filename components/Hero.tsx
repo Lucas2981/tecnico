14e7f019712cd2e66ef60e926f9190333f109384
Lucas2981 <lucas298113@gmail.com>
2024-11-26 21:59:01 -0300

    bg, size image icon

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+import casa_plano from "../public/casa_plano.avif";
+
 const Hero = () => {
   return (
-    <section id="inicio" className="relative bg-cover bg-center bg-no-repeat">
-      <div className="absolute inset-0 bg-gradient-to-r from-gris-30/75 to-gris-20/50"></div>
-      <div className="h-[740px] md:h-[940px] relative mx-auto max-w-screen-xl px-4 py-2 sm:py-24 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+    <section id="inicio" className="relative bg-cover bg-center bg-no-repeat ">
+      <div className="absolute inset-0 bg-gradient-to-r from-gris-30/75 to-gris-20/50 z-10"></div>
+      <div className=" h-[740px] md:h-[940px] relative mx-auto max-w-screen-4xl px-4 py-2 sm:py-24 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+        {/* <div className="border-8 border-verde-10 h-[740px] md:h-[940px] relative mx-auto max-w-screen-xl px-4 py-2 sm:py-24 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"> */}
+        <Image
+          className="absolute inset-0 h-full w-full object-cover object-center grayscale"
+          src={casa_plano}
+          alt="casa"
+          width={0}
+          height={0}
+          sizes="100vw"
+        />
+
         <div className="container relative z-10 mx-auto">
           <h1 className="text-color-h1 px-10 pt-40 md:pt-20 text-start">
             <span className="text-start bold-40 lg:bold-88 text-white">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,17 +17,11 @@ module.exports = {
           30: "#383838",
         },
       },
-      backgroundImage: {
-        "bg-img-1": "url('/img-1.png')",
-        "bg-img-2": "url('/img-2.png')",
-        "feature-bg": "url('/feature-bg.png')",
-        pattern: "url('/pattern.png')",
-        "pattern-2": "url('/pattern-bg.png')",
-      },
       screens: {
         xs: "400px",
         "3xl": "1680px",
         "4xl": "2200px",
+        "5xl": "2560px",
       },
       maxWidth: {
         "10xl": "1512px",
